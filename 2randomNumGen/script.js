544afa98432dd70732fe7dd886ab973d930b27c5
rpremvaree12 <richard.premvaree@gmail.com>
2023-02-24 14:58:19 -0500

    second project

---
+++ 2randomNumGen/script.js
@@ -0,0 +1,12 @@
+const number = document.querySelector(".number")
+const btn = document.querySelector(".generate")
+
+const generateNumber = ()=>{
+    const randomNumber = Math.floor(Math.random() * 10 +1);
+    number.innerHTML = randomNumber;
+}
+
+btn.addEventListener("click", generateNumber);
+
+// place a random number when page loads
+generateNumber();

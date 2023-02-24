189bd63c86d36ac4f48d2d939e7f4ad731c84fe1
rpremvaree12 <richard.premvaree@gmail.com>
2023-02-24 15:13:45 -0500

    cosmetic changes

--- 2randomNumGen/script.js
+++ 2randomNumGen/script.js
@@ -2,7 +2,7 @@ const number = document.querySelector(".number")
 const btn = document.querySelector(".generate")
 
 const generateNumber = ()=>{
-    const randomNumber = Math.floor(Math.random() * 10 +1);
+    const randomNumber = Math.floor(Math.random() * 10)+1;
     number.innerHTML = randomNumber;
 }
 

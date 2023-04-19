70490074a8e61c3c64084c8728fbff5a3dc0eb9f
Richard <richard.premvaree@gmail.com>
2023-04-19 14:14:18 -0400

    internet connection status

--- 10internetConnectionStatus/script.js
+++ 10internetConnectionStatus/script.js
@@ -1,31 +1,38 @@
+const icon = document.querySelector("#statusIcon");
+const statusDisplay = document.querySelector("#status");
+const bgColor = document.querySelector("#main");
+
+function setColor(){
+    bgColor.classList.add("online");
+    icon.classList.add("fa-solid")
+    icon.classList.add("fa-wifi")
+}
+
 window.addEventListener("load",(e)=>{
     const statusDisplay = document.querySelector("#status");
     statusDisplay.textContent = navigator.onLine ? "Online" : "Offline";
+    setColor();
+    // fa-wifi-slash
 })
 
 window.addEventListener("offline",(e)=>{
     const statusDisplay = document.querySelector("#status");
     statusDisplay.textContent = "Offline";
+    setColor();
 })
 
 window.addEventListener("online",(e)=>{
     const statusDisplay = document.querySelector("#status");
     statusDisplay.textContent = "Online";
+    setColor();
 })
 
 // above works even if there's no active internet connection.
 // only if connected to a network
 
-const image = document.querySelector("#image");
-const statusDisplay = document.querySelector("#status");
-const bgColor = document.querySelector("#main");
-
-function setColor(){
-    bgColor.classList.add("online")
-}
 async function connectionStatus(){
     try{
-        const fetchResult = await fetch("https://commons.wikimedia.org/wiki/Main_Page?time=" + (new Date().getTime());
+        const fetchResult = await fetch("https://commons.wikimedia.org/wiki/Main_Page?time=" + (new Date().getTime()));
         image.src = "./images/online.png";
         setColor();
         return fetchResult.status >= 200 && fetchResult.status < 300

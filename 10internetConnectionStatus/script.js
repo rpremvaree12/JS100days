b56e32d6f4079f4e89b4800ef39de9cba247b4f9
rpremvaree12 <richard.premvaree@gmail.com>
2023-04-21 10:30:42 -0400

    fixed errors

--- 10internetConnectionStatus/script.js
+++ 10internetConnectionStatus/script.js
@@ -1,42 +1,64 @@
+// this works even if there's no active internet connection.
+// only if connected to a network - false signal
+
+// // when window loads
+// window.addEventListener("load",(e)=>{
+//     const statusDisplay = document.querySelector("#status");
+//     statusDisplay.textContent = navigator.onLine ? "Online" : "Offline";
+//     setColor();
+//     // fa-wifi-slash
+// })
+
+// // when changes to offline
+// window.addEventListener("offline",(e)=>{
+//     const statusDisplay = document.querySelector("#status");
+//     statusDisplay.textContent = "Offline";
+//     setColor();
+// })
+
+// // when changes to online
+// window.addEventListener("online",(e)=>{
+//     const statusDisplay = document.querySelector("#status");
+//     statusDisplay.textContent = "Online";
+//     setColor();
+// })
 const icon = document.querySelector("#statusIcon");
 const statusDisplay = document.querySelector("#status");
 const bgColor = document.querySelector("#main");
 
-function setColor(){
-    bgColor.classList.add("online");
-    icon.classList.add("fa-solid")
-    icon.classList.add("fa-wifi")
-}
-
-window.addEventListener("load",(e)=>{
-    const statusDisplay = document.querySelector("#status");
-    statusDisplay.textContent = navigator.onLine ? "Online" : "Offline";
-    setColor();
-    // fa-wifi-slash
-})
-
-window.addEventListener("offline",(e)=>{
-    const statusDisplay = document.querySelector("#status");
-    statusDisplay.textContent = "Offline";
-    setColor();
-})
-
-window.addEventListener("online",(e)=>{
-    const statusDisplay = document.querySelector("#status");
-    statusDisplay.textContent = "Online";
-    setColor();
-})
-
-// above works even if there's no active internet connection.
-// only if connected to a network
-
 async function connectionStatus(){
     try{
-        const fetchResult = await fetch("https://commons.wikimedia.org/wiki/Main_Page?time=" + (new Date().getTime()));
-        image.src = "./images/online.png";
-        setColor();
+        const fetchResult = await fetch('https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Lenna_%28test_image%29.png/440px-Lenna_%28test_image%29.png?time='+ (new Date()).getTime());
+        
         return fetchResult.status >= 200 && fetchResult.status < 300
     } catch (error){
-        console.log(error);
+
+        bgColor.classList.remove("online");
+        icon.classList.remove("fa-heart-pulse");
+
+        icon.classList.add("fa-heart-crack");
+        statusDisplay.textContent = "You are OFFLINE";
+
     }
 }
+
+
+setInterval(async ()=>{
+    const result = await connectionStatus();
+    if (result){
+        statusDisplay.textContent = "You are ONLINE";
+        bgColor.classList.add("online");
+        icon.classList.add("fa-heart-pulse")
+    }
+}, 3000);
+
+window.addEventListener("load", async(event)=>{
+    if(connectionStatus()){
+        bgColor.classList.add("online");
+        icon.classList.add("fa-heart-pulse")
+        statusDisplay.textContent = "You are ONLINE";
+    }else{
+        icon.classList.add("fa-heart-crack");
+        statusDisplay.textContent = "You are OFFLINE on load";
+    }
+})
